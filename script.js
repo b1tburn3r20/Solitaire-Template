2c28f9fa043efaad9db843328d45cd04912455b6
b1tburn3r20 <alejandrojosuebermudez@gmail.com>
2023-04-23 18:12:04 -0500

    added pointer on icons & cached elements

---
+++ script.js
@@ -0,0 +1,21 @@
+	/*----- constants -----*/
+
+
+	/*----- state variables -----*/
+
+
+	/*----- cached elements  -----*/
+    resetBtn = document.getElementById('resetButton')
+    undoBtn = document.getElementById('undoButton')
+    deckPlaceholder = document.getElementById('deckPlaceholder')
+    heartsPlaceholder = document.getElementById('heartsPlaceholder')
+    diamondsPlaceholder = document.getElementById('diamondsPlaceholder')
+    clubsPlaceholder = document.getElementById('clubsPlaceholder')
+    spadesPlaceholder = document.getElementById('spadesPlaceholder')
+    score = document.getElementById("score")
+    timer = document.getElementById('timer')
+    sound = document.getElementById('sound')
+	/*----- event listeners -----*/
+
+
+	/*----- functions -----*/
